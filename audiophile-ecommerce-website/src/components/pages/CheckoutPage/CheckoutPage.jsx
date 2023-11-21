a8f3b9175a6bfd13e43e2f89e0628bbec179625f
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2023-11-21 16:07:24 +0100

    CheckoutPage changes
    Changes:
    - Add new inputs and functions

--- audiophile-ecommerce-website/src/components/pages/CheckoutPage/CheckoutPage.jsx
+++ audiophile-ecommerce-website/src/components/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,8 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./checkoutPage.module.scss";
 import { Link } from "react-router-dom";
 
 export const CheckoutPage = () => {
+    const [infoPerson, setInfoPerson] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+        city: "",
+        zip: "",
+        paymentMethod: "e-Money",
+        emoneyNumber: "",
+        emoneyPin: "",
+    });
+    const inputList = {
+        billingDetails: [
+            {
+                id: 0,
+                name: "name",
+                label: "Name",
+                type: "text",
+                placeholder: "Alexei Ward",
+                large: false,
+            },
+            {
+                id: 1,
+                name: "email",
+                label: "Email Address",
+                type: "email",
+                placeholder: "alexei@mail.com",
+                large: false,
+            },
+            {
+                id: 2,
+                name: "phone",
+                label: "Phone Number",
+                type: "tel",
+                placeholder: "+1 202-555-0136",
+                large: false,
+            },
+        ],
+        shippingInfo: [
+            {
+                id: 0,
+                name: "address",
+                label: "Address",
+                type: "text",
+                placeholder: "1137 Williams Avenue",
+                large: true,
+            },
+            {
+                id: 1,
+                name: "zip",
+                label: "ZIP Code",
+                type: "number",
+                placeholder: "01001",
+                large: false,
+            },
+            {
+                id: 2,
+                name: "city",
+                label: "City",
+                type: "text",
+                placeholder: "New York",
+                large: false,
+            },
+        ],
+    };
+
+    const handleCheckout = () => {
+        console.log(infoPerson);
+    };
+    const handleChangeInput = (e) => {
+        setInfoPerson({ ...infoPerson, [e.target.id]: e.target.value });
+    };
+
     return (
         <div className={style.checkoutPage}>
             <div className={style.insideCheckout}>
@@ -18,75 +91,131 @@ export const CheckoutPage = () => {
                                     Billing details
                                 </h4>
                                 <div className={style.contentInputs}>
-                                    <div className={style.itemInputHalf}>
-                                        <label htmlFor="name">Name</label>
-                                        <input
-                                            type="text"
-                                            id="name"
-                                            placeholder="Alexei Ward"
-                                        />
-                                    </div>
-                                    <div className={style.itemInputHalf}>
-                                        <label htmlFor="email">
-                                            Email Address
-                                        </label>
-                                        <input
-                                            type="email"
-                                            id="email"
-                                            placeholder="alexei@mail.com"
-                                        />
-                                    </div>
-                                    <div className={style.itemInputHalf}>
-                                        <label htmlFor="phone">
-                                            Phone number
-                                        </label>
-                                        <input
-                                            type="tel"
-                                            id="name"
-                                            placeholder="+1 202-555-0136"
-                                        />
-                                    </div>
+                                    {inputList.billingDetails.map((item) => (
+                                        <div
+                                            className={`${
+                                                item.large
+                                                    ? style.itemInputFull
+                                                    : style.itemInputHalf
+                                            }`}
+                                            key={item.id}
+                                        >
+                                            <label htmlFor={item.name}>
+                                                {item.label}
+                                            </label>
+                                            <input
+                                                onChange={(event) =>
+                                                    handleChangeInput(event)
+                                                }
+                                                type={item.type}
+                                                id={item.name}
+                                                placeholder={item.placeholder}
+                                            />
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                             <div className={style.shippingInfo}>
                                 <h4 className={style.title4}>Shipping info</h4>
                                 <div className={style.contentInputs}>
-                                    <div className={style.itemInputFull}>
-                                        <label htmlFor="address">Address</label>
-                                        <input
-                                            type="text"
-                                            id="address"
-                                            placeholder="1137 Williams Avenue"
-                                        />
-                                    </div>
-                                    <div className={style.itemInputHalf}>
-                                        <label htmlFor="zip">ZIP Code</label>
-                                        <input
-                                            type="number"
-                                            id="zip"
-                                            placeholder="01001"
-                                        />
-                                    </div>
-                                    <div className={style.itemInputHalf}>
-                                        <label htmlFor="City">City</label>
-                                        <input
-                                            type="text"
-                                            id="city"
-                                            placeholder="01001"
-                                        />
-                                    </div>
+                                    {inputList.shippingInfo.map((item) => (
+                                        <div
+                                            className={`${
+                                                item.large
+                                                    ? style.itemInputFull
+                                                    : style.itemInputHalf
+                                            }`}
+                                            key={item.id}
+                                        >
+                                            <label htmlFor={item.name}>
+                                                {item.label}
+                                            </label>
+                                            <input
+                                                onChange={(event) =>
+                                                    handleChangeInput(event)
+                                                }
+                                                type={item.type}
+                                                id={item.name}
+                                                placeholder={item.placeholder}
+                                            />
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                             <div className={style.paymentsDetails}>
                                 <h4 className={style.title4}>
                                     Payment details
                                 </h4>
+                                <div className={style.inputContent}>
+                                    <div className={style.paymentType}>
+                                        <p className={style.labelPaymentType}>
+                                            Payment Method
+                                        </p>
+                                        <div className={style.selectorPaymentType}>
+                                            <button
+                                                className={`${
+                                                    infoPerson.paymentMethod ===
+                                                    "e-Money"
+                                                        ? style.activeButtonType
+                                                        : ""
+                                                } ${style.buttonPaymentType}`}
+                                                onClick={(event) =>
+                                                    handleChangeInput(event)
+                                                }
+                                                value="e-Money"
+                                                id="paymentMethod"
+                                            >
+                                                <div
+                                                    className={style.point}
+                                                ></div>
+                                                <p>e-Money</p>
+                                            </button>
+                                            <button
+                                                className={`${
+                                                    infoPerson.paymentMethod ===
+                                                    "cash"
+                                                        ? style.activeButtonType
+                                                        : ""
+                                                } ${style.buttonPaymentType}`}
+                                                value={"cash"}
+                                                id="paymentMethod"
+                                                onClick={(event) =>
+                                                    handleChangeInput(event)
+                                                }
+                                            >
+                                                <div
+                                                    className={style.point}
+                                                ></div>
+                                                <p>Cash on Delivery</p>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className={style.summaryCheckout}>
                         <h3 className={style.title3SummaryCheckout}>Summary</h3>
-                        <button className={style.checkoutButton}>chekout</button>
+                        <div className={style.totalContent}>
+                            <div className={style.itemTotal}>
+                                <p>Total</p>
+                                <p className={style.price}>$ 0.00</p>
+                            </div>
+                            <div className={style.itemTotal}>
+                                <p>Shipping</p>
+                                <p className={style.price}>$ 0.00</p>
+                            </div>
+                            <div className={style.itemTotal}>
+                                <p>VAT (included)</p>
+                                <p className={style.price}>$ 0.00</p>
+                            </div>
+                            <button
+                                className={style.checkoutButton}
+                                onClick={handleCheckout}
+                            >
+                                checkout
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
